70166685d164d9403be5336dbb3b301a38bff671
brandonb <brandonb.devs@gmail.com>
2022-05-13 13:14:22 -0500

    Commit Inicial

---
+++ js/app.js
@@ -0,0 +1,99 @@
+(function(){
+  const app = document.getElementById('app');
+  let inputCaracteres = document.getElementById('numero-caracteres');
+
+  let configuracion = {
+    caracteres : parseInt(inputCaracteres.value),
+    simbolos : true,
+    numeros : true,
+    mayusculas : true,
+    minusculas : true
+  }
+
+  let caracteres = {
+    numeros : '0 1 2 3 4 5 6 7 8 9',
+    simbolos : '! # $ % & ( ) * + , - . / : ; < = > ? @ [ ] ^ _ { | } ~ ',
+    mayusculas : 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
+    minusculas : 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
+  }
+
+  // Eventos
+  app.addEventListener('submit', function(e){
+    e.preventDefault();
+  });
+
+  app.elements.namedItem('btn-mas-uno').addEventListener('click', function(){
+    configuracion.caracteres += 1;
+    inputCaracteres.value = configuracion.caracteres;
+  });
+
+
+  app.elements.namedItem('btn-menos-uno').addEventListener('click', function(){
+    if(configuracion.caracteres > 1){
+      configuracion.caracteres -= 1;
+      inputCaracteres.value = configuracion.caracteres;
+    }
+  });
+
+  // Funciones
+  app.elements.namedItem('btn-simbolos').addEventListener('click', function(){
+    btnToggle(this);
+    configuracion.simbolos = !configuracion.simbolos;
+  });
+
+  app.elements.namedItem('btn-numeros').addEventListener('click', function(){
+    btnToggle(this);
+    configuracion.numeros = !configuracion.numeros;
+  });
+
+  app.elements.namedItem('btn-mayusculas').addEventListener('click', function(){
+    btnToggle(this);
+    configuracion.mayusculas = !configuracion.mayusculas;
+  });
+
+  app.elements.namedItem('btn-generar').addEventListener('click', function(){
+    generarPassword();
+  })
+
+
+  app.elements.namedItem('input-password').addEventListener('click', function(){
+    copiarPassword();
+  })
+
+  function btnToggle(element){
+    element.childNodes[0].classList.toggle('fa-check');
+    element.childNodes[0].classList.toggle('fa-times');
+  }
+
+  function generarPassword(){
+    let caracteresFinales = '';
+    let password = '';
+    for(propiedad in configuracion){
+      if(configuracion[propiedad] == true){
+        caracteresFinales += caracteres[propiedad] + ' ';
+        
+      }
+    }
+    caracteresFinales = caracteresFinales.trim();
+    caracteresFinales = caracteresFinales.split(' ');
+
+    for(let i = 0; i< configuracion.caracteres; i++){
+      password +=  caracteresFinales[Math.floor(Math.random() * caracteresFinales.length)];
+    }
+    app.elements.namedItem('input-password').value = password;
+    
+  }
+
+  function copiarPassword() {
+    app.elements.namedItem('input-password').select();
+    document.execCommand('copy');
+    document.getElementById('alerta-copiado').classList.add('active');    
+
+    setTimeout(()=>{
+      document.getElementById('alerta-copiado').classList.remove('active');
+    },1000);
+  }
+
+  generarPassword();
+
+}());
